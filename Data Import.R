f7ba3ec8dc900cd4196cbb2fff04135455101e18
Adam Ficke <aficke@gmail.com>
2020-02-11 18:29:55 -0500

    Data Import Code

---
+++ Data Import.R
@@ -0,0 +1,5 @@
+# Code to conduct EDA on Electric power consumption dataset for Coursera course
+#Author: Adam Ficke
+
+#Import File 
+electric <- read.table("household_power_consumption.txt",header = TRUE,sep=";")
