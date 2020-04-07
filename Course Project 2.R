e87a291431239e79cf20af523652a9b1061354fe
Adam Ficke <aficke@gmail.com>
2020-04-07 11:13:35 -0400

    Question 1 done

--- Course Project 2.R
+++ Course Project 2.R
@@ -29,6 +29,16 @@ par(mfrow = c(1,1))
 ggplot(meg1, aes(x=factor(year), y=Emissions)) + stat_summary(fun.y="sum", geom="line")
 
 
+#get dates out 
+dates <- mrg1$year
+plot(dates,mrg1$Emissions)
+SumYear <- with(mrg1, tapply(Emissions,year,sum,na.rm=T))
+dim(SumYear)
+d0 <- data.frame(Year = as.Date(names(SumYear),"%Y"),Sum = SumYear)
+plot(d0$Year,d0$Sum)
+
+     #,xlim = as.Date(c("1998-04-07","2009-04-07")))
+str(d0)
 
 
 with(NEI,plot(as.Date(NEI$year),NEI$Emissions)
